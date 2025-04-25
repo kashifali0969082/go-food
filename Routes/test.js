89d61bfc88e99dea5998dc53a4dde365a2910706
kashifali0969082 <kashifali0969082@gmail.com>
2025-04-26 01:12:05 +0500

    uo

---
+++ Routes/test.js
@@ -0,0 +1,12 @@
+const Order = require('../models/Orders');
+
+router.get('/testing', async (req, res) => {
+    try {
+      const [rows] = await db.query('SELECT * FROM orders');
+      res.status(200).json({ success: true, data: rows });
+    } catch (err) {
+      console.error('Error fetching orders:', err);
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+  module.exports = router;  
